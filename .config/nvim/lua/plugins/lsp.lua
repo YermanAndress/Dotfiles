fcc5e04bd59114855c85bafeb1d840fe59044834
YermanAndress <geranroca1120@gmail.com>
2026-02-03 21:59:36 -0500

    Add: Nvim config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,48 +1,31 @@
 return {
   "neovim/nvim-lspconfig",
+  event = { "BufReadPost", "BufNewFile" }, -- Agregada coma aquí
   dependencies = {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp", -- Necesario para las capabilities
+    "hrsh7th/cmp-nvim-lsp",
   },
   config = function()
     require("mason").setup()
 
-    -- 1. Definimos las capacidades del autocompletado una sola vez
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
     
     require("mason-lspconfig").setup({
       ensure_installed = {
         "lua_ls", "pyright", "bashls", "yamlls", "jsonls",
-        "ts_ls", "html", "cssls", "jdtls", "stsll",
+        "ts_ls", "html", "cssls",
       },
-      
       handlers = {
-        -- 2. Configuración por defecto para todos los servidores
         function(server_name)
           require("lspconfig")[server_name].setup({
             capabilities = capabilities,
           })
         end,
-        
-        -- 3. Configuración específica para Java
-        ["jdtls"] = function()
-          require("lspconfig").jdtls.setup({
-            capabilities = capabilities,
-            -- Aquí puedes añadir configuraciones específicas de Java más adelante
-          })
-        end,
-
-        -- 4. Configuración específica para Spring Boot (Importante)
-        ["stsll"] = function()
-          require("lspconfig").stsll.setup({
-            capabilities = capabilities,
-          })
-        end,
-      },
+      }, 
     })
 
-    -- 5. Atajos de teclado cuando el LSP se conecta
+    -- Autocomando de LspAttach (Atajos de teclado)
     vim.api.nvim_create_autocmd("LspAttach", {
       callback = function(ev)
         local opts = { buffer = ev.buf }
@@ -52,7 +35,6 @@ return {
         map("n", "gi", vim.lsp.buf.implementation, opts)
         map("n", "<leader>rn", vim.lsp.buf.rename, opts)
         map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        -- Sugerencia: añadir un atajo para ver errores flotantes
         map("n", "<leader>e", vim.diagnostic.open_float, opts)
       end,
     })
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  ft = { "lua", "vim", "vimdoc", "bash", "markdown", "python", 
+        "javascript", "typescript", "html", "css", "c", "rust" }, 
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "bash",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "javascript",
-        "typescript",
-        "html",
-        "css",
-        "json",
-        "c",
-        "cpp",
-        "yaml",
-        "toml",
-        "go",
-        "rust",
-        "java",
-        "kotlin",
-        "ruby",
-        "sql"
-      },
       highlight = { enable = true },
       indent = { enable = true },
     })
