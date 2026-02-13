399221658f8a8bab50144425d18bd066697089e4
YermanAndress <geranroca1120@gmail.com>
2026-02-12 21:47:19 -0500

    Update: Fix Config & Home_Path

--- Documentos/Backup/install-VM.sh
+++ Documentos/Backup/install-VM.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # --- Configuración VM ---
-DOTFILES_REPO="https://github.com/YermanAndress/Dotfiles.git"
-PACKAGES="zsh bat eza ripgrep thunar base-devel git ttf-firacode-nerd ttf-nerd-fonts-symbols-common nwg-look rsync virtualbox-guest-utils"
+PACKAGES="zsh bat eza ripgrep thunar git ttf-firacode-nerd ttf-nerd-fonts-symbols-common nwg-look rsync virtualbox-guest-utils"
 AUR_PACKAGES="zen-browser-bin pokeget sddm-theme-tokyo-night visual-studio-code-bin"
 
 echo "🖥️ Iniciando instalación en Máquina Virtual..."
@@ -11,18 +10,21 @@ WORK_DIR=$(mktemp -d)
 
 # 1. Actualizar e instalar base
 sudo pacman -Syu --noconfirm
-sudo pacman -S --needed --noconfirm $PACKAGES
+sudo pacman -S --needed base-devel --noconfirm $PACKAGES
 
 # Activar servicios de VM inmediatamente
 sudo systemctl enable --now vboxservice.service
+sudo systemctl --user enable --now dbus-broker.service
+
+sudo usermod -aG video $USER
+
 
 # 2. Instalar Paru
-if ! command -v paru &> /dev/null; then
-    echo "📦 Instalando Paru..."
-    git clone https://aur.archlinux.org/paru-bin "$WORK_DIR/paru"
-    cd "$WORK_DIR/paru" && makepkg -si --noconfirm
-    cd -
-fi
+echo "📦 Instalando Paru..."
+git clone https://aur.archlinux.org/paru "$WORK_DIR/paru"
+cd "$WORK_DIR/paru" && makepkg -si --noconfirm
+cd -
+
 
 curl -fsSL https://install.danklinux.com | sh
 
@@ -30,29 +32,31 @@ curl -fsSL https://install.danklinux.com | sh
 paru -S --noconfirm $AUR_PACKAGES
 
 # 4. Starship y Shell
-curl -sS https://starship.rs/install.sh | sh -s -- -y
-[ "$SHELL" != "/usr/bin/zsh" ] && chsh -s $(which zsh)
+curl -sS https://starship.rs/install.sh | sh
+sudo chsh -s $(which zsh) $USER
 
 # 6. Desplegar Dotfiles (Git Bare)
-echo "📂 Clonando configuraciones..."
-git clone --separate-git-dir=$HOME/.cfg -b main "$DOTFILES_REPO" $HOME/tmp_dotfiles
-rsync --recursive -vhP $HOME/tmp_dotfiles/ $HOME/
-rm -rf $HOME/tmp_dotfiles
+#echo "📂 Clonando configuraciones..."
+#git clone --separate-git-dir=$HOME/.cfg -b main "$DOTFILES_REPO" $HOME/tmp_dotfiles
+#rsync --recursive -vhP $HOME/tmp_dotfiles/ $HOME/
+#rm -rf $HOME/tmp_dotfiles
 
 # 8. Restaurar archivos de sistema (Adaptado a VM)
 echo "🔧 Restaurando configuraciones..."
-BACKUP_PATH="$HOME/Dotfiles/Documentos/Backup/SystemBackups/etc"
+SYSTEM_BACKUP_PATH="$HOME/Dotfiles/Documentos/Backup/SystemBackups/etc"
+CONFIG_BACKUP_PATH="$HOME/Dotfiles/"
 
-if [ -d "$BACKUP_PATH" ]; then
+
+if [ -d "$SYSTEM_BACKUP_PATH" ]; then
     # Solo copiamos lo que tiene sentido en una VM (pacman, mkinitcpio)
     # NO copiamos fstab ni network si la VM usa NAT/Bridge estándar
-    sudo cp -f "$BACKUP_PATH/mkinitcpio.conf" /etc/mkinitcpio.conf
-    sudo cp -f "$BACKUP_PATH/pacman.conf" /etc/pacman.conf
+    sudo cp -f "$SYSTEM_BACKUP_PATH/mkinitcpio.conf" /etc/mkinitcpio.conf
+    sudo cp -f "$SYSTEM_BACKUP_PATH/pacman.conf" /etc/pacman.conf
     
     # El polkit sigue siendo útil si montas carpetas compartidas
-    if [ -f "$BACKUP_PATH/polkit-1/rules.d/99-udisks2.rules" ]; then
+    if [ -f "$SYSTEM_BACKUP_PATH/polkit-1/rules.d/99-udisks2.rules" ]; then
         sudo mkdir -p /etc/polkit-1/rules.d
-        sudo cp -f "$BACKUP_PATH/polkit-1/rules.d/99-udisks2.rules" /etc/polkit-1/rules.d/
+        sudo cp -f "$SYSTEM_BACKUP_PATH/polkit-1/rules.d/99-udisks2.rules" /etc/polkit-1/rules.d/
     fi
 
     sudo mkinitcpio -P
@@ -61,10 +65,23 @@ else
     echo "⚠️ No se encontró SystemBackups."
 fi
 
+
+if [ -d "$CONFIG_BACKUP_PATH" ]; then
+
+    cp -f "$CONFIG_BACKUP_PATH/.config" ~ -r
+    cp -f "$CONFIG_BACKUP_PATH/Descargas" ~ -r
+    cp -f "$CONFIG_BACKUP_PATH/Descargas" ~ -r
+    cp -f "$CONFIG_BACKUP_PATH/Pictures" ~ -r
+    cp -f "$CONFIG_BACKUP_PATH/Scripts" ~ -r
+    cp -f "$CONFIG_BACKUP_PATH/.zshrc" ~ -r
+
+fi
+
 rm -rf "$WORK_DIR"
 
 # Alias permanente
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+echo "alias config='/usr/bin/git --git-dir=\$HOME/.cfg/ --work-tree=\$HOME'" >> ~/.zshrc
+
 config config --local status.showUntrackedFiles no
 
 echo "✨ ¡Hecho! VM lista. Reinicia para activar los drivers de invitado."
